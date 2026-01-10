4e9a3d03ace8a1abbc62f8f82880ace7a46eec2a
Vikash <vikashyadav5131@gmail.com>
2026-01-10 23:35:58 +0530

    handle API response

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let h1 = document.querySelector(".temprature-details").querySelector('.heading1');
+let tempratue = document.querySelector(".temprature-details").querySelector('h1');
+
+let cityinput = document.querySelector('.city-input');
 
 let h2 = document.querySelector('.temprature-details').querySelector('.heading2');
 
@@ -8,5 +10,29 @@ let windPara = document.querySelector('.wind_details');
 
 let btn = document.querySelector('button');
 
-let BASE_URL = `https://api.weatherstack.com/current?access_key=03fc81745ffeb2e031b8308a7d32da85&query=${CityName}`
+let Base_URL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore';
+
+let API_KEY = '1be2e36560ec3fd4cdadfad278eeac83';
+
+btn.addEventListener('click', (evt) =>{
+    checkWeather(cityinput.value);
+})
+
+
+
+
+async function checkWeather(){
+    let response = await fetch(Base_URL + `&appid=${API_KEY}`);
+    let data = response.json();
+
+    humidityPara.innerHTML = data
+    cityinput.innerHTML = 
+    windPara.innerHTML = 
+    tempratue.innerHTML = 
+
+}
+
+checkWeather();
+
+
 
